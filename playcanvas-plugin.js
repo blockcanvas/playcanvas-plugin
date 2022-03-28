f83da7b8fd9d20435067c2d27ffd24e8f4a58405
quantumgames-studiosinc <98978887+quantumgames-studiosinc@users.noreply.github.com>
2022-03-28 14:52:09 -0600

    Update playcanvas-plugin.js

--- playcanvas-plugin.js
+++ playcanvas-plugin.js
@@ -14,24 +14,7 @@ let yml = `
           function: initiliaze
           code: > 
            func initiliaze(a) {
-           "var Rifts = pc.createScript('rifts');
-
-// initialize code called once per entity
-Rifts.prototype.initialize = function() {
-
-};
-
-// update code called every frame
-Rifts.prototype.update = function(dt) {
-
-};
-
-// swap method called for script hot-reloading
-// inherit your script state here
-// Rifts.prototype.swap = function(old) { };
-
-// to learn more about script anatomy, please read:
-// https://developer.playcanvas.com/en/user-manual/scripting/ "
+           
             
            }
 
