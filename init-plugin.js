fe95e43ab08168a7556b39bcdd8958496f62c2cd
quantumgames-studiosinc <98978887+quantumgames-studiosinc@users.noreply.github.com>
2022-03-28 13:19:10 -0600

    Update init-plugin.js

--- init-plugin.js
+++ init-plugin.js
@@ -1,32 +1,27 @@
+console.log('hey! plugin js code executed')
 let yml = `
 ---
-//ver=0.0.2
-  blockcanvas: #name of the category
-    color: ff0000 #color of the category
-    icon: text.svg #icon of the category
+  Test:
+    color: ff0000
+    icon: text.svg
     blocks:
-      - playcanvasinit: #there is a block that we call it test
+      - test:
           shadows:
-            - 1 #it has one shadow(Whenever no other value is present in the input, this value fills it)
+            - 1
           args:
-            a: statement #we have two inputs one of them gives a normal value and the other one gives a program, a statement(as a lambda function)
-          function: init #the function that we call when this block is used. you can see definition of it in the next line
+            a: value
+            b: statement
+          function: test
           code: >
-var Script = pc.createScript('script');
-
-// initialize code called once per entity
-Script.prototype.initialize = function() {
-
-};
-
-// update code called every frame
-Script.prototype.update = function(dt) {
-
-};
-          messages: #The messages we want to appear on the block (we put them before inputs)
-            - playcanvasinit
+            func test(a, b) {
+              print(a)
+              b!()
+            }
+          messages:
+            - test
             -
-          hasReturn: false #is it a block that returns something (ellipse ones)
-          tooltip: test #the tooltip of the block
+          hasReturn: false
+          tooltip: test
+          ver=1.0.1
 `
 createBlocksFromYAML(yml)
