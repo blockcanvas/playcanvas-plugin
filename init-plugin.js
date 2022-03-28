319ba7e8e7d97d23f303529dc5f27e93a5c07d9a
quantumgames-studiosinc <98978887+quantumgames-studiosinc@users.noreply.github.com>
2022-03-28 13:37:18 -0600

    Update init-plugin.js

--- init-plugin.js
+++ init-plugin.js
@@ -13,10 +13,17 @@ let yml = `
             a: statement
           function: initilize
           code: >
-            func test(a, b) {
-              print(a)
-              b!()
-            }
+            var Script = pc.createScript('script');
+
+// initialize code called once per entity
+Script.prototype.initialize = function() {
+
+};
+
+// update code called every frame
+Script.prototype.update = function(dt) {
+
+};
           
           messages:
             - initalize
